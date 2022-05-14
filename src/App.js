959d4ccd26fd878ace40648380f6d9a3a0f91f15
mansi-done <mansi.saini01@gmail.com>
2022-05-14 15:55:34 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Routes , Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Layout,Typography, Space } from 'antd';
+
+import { Navbar , Homepage , Exchanges, Cryptocurrencies,CryptoDetails,News } from './components';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div  className="app">
+        <div className="navbar">
+            <Navbar/>
+        </div>
+        <div className="main">
+          <Layout>
+            <div className='routes'>
+               <Routes>
+                <Route exact path='/' element = {<Homepage/>}/>
+                <Route exact path='/exchanges' element = {<Exchanges/>}/>
+                <Route exact path='/cryptocurrencies' element = {<Cryptocurrencies/>}/>
+                <Route exact path='/crypto/:coinID' element = {<CryptoDetails/>}/>
+                <Route exact path='/news' element = {<News/>}/>
+              </Routes> 
+            </div>
+          </Layout>
+        
+        <div className="footer">
+          
+          <Space>
+            <Link to='/'> Home </Link>
+            <Link to='/cryptocurrencies'> Cryptocurrencies </Link>
+            <Link to='/news'> News </Link>
+          </Space>
+
+          <Typography.Title level={5} style = {{color :"white", textAlign:"center"}}>
+            Crypto Den <br/>
+            © All rights reserved <br/>
+            Built by Mansi Saini 
+          </Typography.Title>
+
+        </div>
+        </div>
+
+        
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
