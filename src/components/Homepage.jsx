d44cc2f12d5908c4612c433542457870a02d9f56
mansi-done <mansi.saini01@gmail.com>
2023-04-18 20:13:39 +0530

    newhome

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,49 +1,89 @@
-import React from 'react';
-import millify from 'millify';
-import { Typography, Row, Col , Statistic , Card } from 'antd';
-import { Link } from 'react-router-dom';
-import Loader from './Loader';
+import React from "react";
+import millify from "millify";
+import { Typography, Row, Col, Statistic, Card } from "antd";
+import { Link } from "react-router-dom";
+import Loader from "./Loader";
 
-import { useGetCryptosQuery } from '../services/cryptoApi';
-import {Cryptocurrencies, News }from '../components' ;
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { Cryptocurrencies, News } from "../components";
 
 const { Title } = Typography;
 
 const Homepage = () => {
-
   const { data, isFetching } = useGetCryptosQuery(10);
 
   const globalStats = data?.data?.stats;
   console.log(data);
 
-  if(isFetching) return <Loader/>;
+  if (isFetching) return <Loader />;
   return (
-    <>
-    <Card>
-    <Title level={3} className='heading'> Global Crypto Stats</Title>
-    <Row>
-      <Col span={12}> <Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
-      <Col span={12}> <Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
-      <Col span={12}> <Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
-      <Col span={12}> <Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
-      <Col span={12}> <Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
-    </Row>
-    </Card>
-    
+    <div className="home">
+      <Card >
+        <Title level={3} className="heading"  >
+          {" "}
+          Global Crypto Stats
+        </Title>
+        <Row>
+          <Col span={12} >
+            {" "}
+            <Statistic
+              title="Total Cryptocurrencies"
+              value={globalStats.total}
+            />
+          </Col>
+          <Col span={12}>
+            {" "}
+            <Statistic
+              title="Total Exchanges"
+              value={millify(globalStats.totalExchanges)}
+            />
+          </Col>
+          <Col span={12}>
+            {" "}
+            <Statistic
+              title="Total Market Cap"
+              value={millify(globalStats.totalMarketCap)}
+            />
+          </Col>
+          <Col span={12}>
+            {" "}
+            <Statistic
+              title="Total 24h Volume"
+              value={millify(globalStats.total24hVolume)}
+            />
+          </Col>
+          <Col span={12}>
+            {" "}
+            <Statistic
+              title="Total Markets"
+              value={millify(globalStats.totalMarkets)}
+            />
+          </Col>
+        </Row>
+      </Card>
 
-    <div className='home-heading-container'>
-      <Title level={2} className="home-title"> Top 10 Cryptocurrencies in the world</Title>
-      <Title level={3} className="show-more"><Link to = "/cryptocurrencies">Show More</Link></Title>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          {" "}
+          Top 10 Cryptocurrencies in the world
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show More</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          {" "}
+          Latest Crypto News
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/news">Show More</Link>
+        </Title>
+      </div>
+      <News simplified />
     </div>
-    <Cryptocurrencies simplified/>
-    <div className='home-heading-container'>
-      <Title level={2} className="home-title"> Latest Crypto News</Title>
-      <Title level={3} className="show-more"><Link to = "/news">Show More</Link></Title>
-    </div>
-    <News simplified/>
+  );
+};
 
-    </>
-  )
-}
-
-export default Homepage
+export default Homepage;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,60 +1,70 @@
-import React from 'react';
-import {Button, Menu , Typography , Avatar , Row , Col} from 'antd';
-import { Link } from 'react-router-dom';
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined , FundOutlined , MenuOutlined } from '@ant-design/icons';
-import icon from '../images/cryptocurrency.png';
-import { useEffect, useState } from 'react';
-
+import React from "react";
+import { Button, Menu, Typography, Avatar, Row, Col } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+import icon from "../images/cryptocurrency.png";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
 
-    const [activeMenu , setActiveMenu] = useState(true);
-    const [screenSize , setScreenSize] = useState(null);
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-    useEffect(()=>{
-        const handleResize = () => setScreenSize(window.innerWidth);
-        window.addEventListener('resize',handleResize);
-        handleResize();
-        return () =>window.removeEventListener('resize',handleResize);
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
 
-    },[]);
+  return (
+    <div className="navbar-wrap">
+      <div className="logo-container">
+        <Avatar src={icon} size="large" />
+        <Typography.Title level={2} className="logo">
+          <a href="/" style={{ textDecoration: "none", color: "white" }}>
+            CryptoDen
+          </a>
+        </Typography.Title>
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
+      </div>
 
-    useEffect(() => {
-        if (screenSize <= 800) {
-          setActiveMenu(false);
-        } else {
-          setActiveMenu(true);
-        }
-      }, [screenSize]);
-
-    return (
-    <div className='nav-container'>
-        <div className='logo-container'>
-            <Avatar src={icon} size = "large" />
-            <Typography.Title level={2} className="logo">
-                <a href='/'>CryptoDen</a>
-            </Typography.Title>
-            <Button className='menu-control-container' onClick={()=>setActiveMenu(!activeMenu)}>
-                <MenuOutlined/>
-            </Button>
-        </div>
-        
-        {activeMenu && (
-            <Menu style={{backgroundColor:'#0c0720'}} theme="dark">
-            <Menu.Item icon = {<HomeOutlined/>}>
-                <Link to='/'>Home</Link>
-            </Menu.Item>
-            <Menu.Item icon = {<FundOutlined/>}>
-                <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
-            </Menu.Item>
-            <Menu.Item icon = {<BulbOutlined/>}>
-                <Link to='/news'>News</Link>
-            </Menu.Item>
-            </Menu>
-        )}
-        
+      {activeMenu && (
+        <Menu
+          theme="dark"
+          style={{ display: screenSize>800 ? "flex" : "default", backgroundColor: "#1f1b24" }}
+        >
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
